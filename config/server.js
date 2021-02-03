4b49ccd7063af41a59bcc2cb4b40c636260a6495
Pablo Bautista <pablo@pixela.com.gt>
2021-02-03 11:39:22 -0600

    Project init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '425dcca738c650d91ff4922aaa9e03af'),
+    },
+  },
+});
