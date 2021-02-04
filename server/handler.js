a230c6a707dc176fcf708300f8fd9695a8f09eae
Pablo Bautista <pablo@pixela.com.gt>
2021-02-04 10:57:23 -0600

    use strict for handler

--- server/handler.js
+++ server/handler.js
@@ -1,11 +1,13 @@
+'use strict'
+
 // Import Strapi and serverless-http
-const strapi = require("strapi/lib/Strapi");
-const serverless = require("serverless-http");
+const strapi = require('strapi/lib/Strapi');
+const serverless = require('serverless-http');
 
 /// Handler for requests
 module.exports.handler = async (event, context) => {
   if (!global.strapi) {
-    console.log("Cold starting Strapi");
+    console.log('Cold starting Strapi');
     await strapi({ dir: __dirname }).start();
   }
   const handler = serverless(global.strapi.app);
